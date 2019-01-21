809b42fb7e6b78054fbae2796f8ea040f19febef
JingyiJia <jiaj@rose-hulman.edu>
2019-01-20 20:47:01 -0500

    Done

--- src/m8_still_more_mutation.py
+++ src/m8_still_more_mutation.py
@@ -65,14 +65,18 @@ def RETURN_delete_negatives(numbers):
       :type numbers: list
     where the list is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # Done: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
     #             m6_mutation.run_test(...)
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
-
+    sequence = []
+    for k in range(len(numbers)):
+        if numbers[k]>=0:
+            sequence.append(numbers[k])
+    return sequence
 
 def run_test_MUTATE_delete_negatives():
     """ Tests the   MUTATE_delete_negatives   function. """
@@ -138,7 +142,10 @@ def MUTATE_delete_negatives(numbers):
     #       then ask for help as needed.
     # HINT #2: Why might it be wise to start at the end and
     #       work backwards through the list to the beginning?
-
+    for k in range(len(numbers)-1,-1,-1):
+        if numbers[k]<0:
+            numbers.remove(numbers[k])
+    return None
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
